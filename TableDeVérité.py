096ec9ba21e9059d37f48d45ecde1d7da6c740b0
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-14 17:05:18 +0300

    chore: update TableDeVérité()

--- TableDeVérité.py
+++ TableDeVérité.py
@@ -7,21 +7,23 @@ class TableDeVérité():
     def ou(self, valeur1, valeur2):
         if valeur1 == "vraie" or valeur2 == "vraie":
             return "vraie"
-        elif valeur1 == "fausse" and valeur2 == "fausse":
-            return "fausse"
+        elif valeur1 == valeur2:
+            return valeur1
         else:
             return "je ne sais pas"
 
     def et(self, valeur1, valeur2):
-        if valeur1 == "vraie" and valeur2 == "vraie":
-            return "vraie"
+        if valeur1 == valeur2:
+            return valeur1
         elif valeur1 == "fausse" or valeur2 == "fausse":
             return "fausse"
         else:
             return "je ne sais pas"
 
     def donc(self, valeur1, valeur2):
-        if valeur1 == "vraie" and valeur2 == "fausse":
+        if valeur1 == valeur2:
+            return "vraie"
+        elif valeur1 != valeur2:
             return "fausse"
         else:
-            return "vraie"
+            return "je ne sais pas"
